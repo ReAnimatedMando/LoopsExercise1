2614dcfd7f8d339a9e8d26e535d8eb43b73f5f0f
Aaron Tanguma <the_guma@yahoo.com>
2025-12-15 09:44:32 -0700

    practiced for loops by writing the example fifteen times

--- LoopsExercise1/Program.cs
+++ LoopsExercise1/Program.cs
@@ -4,11 +4,79 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i <= 5; i++)
         {
             Console.WriteLine($"{i}");
         }
-        
-        
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
     }
 }
